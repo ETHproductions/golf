0486501b7a402688e7755820b4e2bee155e8b2c8
ETHproductions <eth3792@nycap.rr.com>
2017-10-18 17:40:06 -0400

    tic-tac-toe: switch button/br to a/p (-10, thanks xem)

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,1 @@
-for(i=12;--i;d.write(i%4?`<button onclick="innerText=d[${j=i*3>>2}]=d[${j}]||'xo'[i++%2];for(a of[0,12,24,2,6,10,3,9])(b=a%4+1,c=d[a>>=2])&&c==d[a+=b]&c==d[a+b]&&d.write(c)">_</button>`:"<br>"))d=document
+for(i=12;--i;d.write(i%4?`<a onclick="innerText=d[${j=i*3>>2}]=d[${j}]||'xo'[i++%2];for(a of[0,12,24,2,6,10,3,9])(b=a%4+1,c=d[a>>=2])&&c==d[a+=b]&c==d[a+b]&&d.write(c)">_</a> `:"<p>"))d=document
