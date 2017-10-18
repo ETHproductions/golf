13af94b1a7cf8b24986bd5a01a95b6f26479aeab
ETHproductions <eth3792@nycap.rr.com>
2017-10-18 13:31:58 -0400

    tic-tac-toe: re-fix previous golf

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,1 @@
-for(i=12;--i;d.write(i%4?`<button onclick="'_'<${T="innerText"}?0:${T}=d[${i*3>>2}]='xo'[i++%2];for(a of[0,12,24,2,6,10,3,9])(b=a%4+1,c=d[a>>=2])&&c==d[a+=b]&c==d[a+b]?d.body[T]=c+' won':0">_</button>`:"<br>"))d=document
++for(i=12;--i;d.write(i%4?`<button onclick="'_'<innerText?0:innerText=d[${i*3>>2}]='xo'[i++%2];for(a of[0,12,24,2,6,10,3,9])(b=a%4+1,c=d[a>>=2])&&c==d[a+=b]&c==d[a+b]&&d.write(c)">_</button>`:"<br>"))d=document
