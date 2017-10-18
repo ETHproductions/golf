767d1c804adbd2476cd033be7c133815daa6809a
ETHproductions <eth3792@nycap.rr.com>
2017-10-18 13:13:00 -0400

    tic-tac-toe: on win, replace everything with winner (-31)

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,1 @@
-for(i=p=12;--i;d.write(i%4?`<button onclick=${T="innerText"}<'_'?0:${T}=d[${i*3>>2}]='XO'[p++%2];for((a)of[0,12,24,2,6,10,3,9])for((q)of(b=a%4+1,c=d[a=a/4|0])&&c==d[a+=b]&c==d[a+b]?d.getElementsByTagName('button'):[])q[T]=c>_</button>`:"<br>"))d=document
+for(i=p=12;--i;d.write(i%4?`<button onclick="'_'<${T="innerText"}?0:${T}=d[${i*3>>2}]='xo'[p--%2];for(a of[0,12,24,2,6,10,3,9])(b=a%4+1,c=d[a>>=2])&&c==d[a+=b]&c==d[a+b]&&d.write(c)">_</button>`:"<br>"))d=document
