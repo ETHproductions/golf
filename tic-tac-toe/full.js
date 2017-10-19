b51e1a5d279ec32fe10099e185a33e3a8e464704
ETHproductions <eth3792@nycap.rr.com>
2017-10-18 22:41:55 -0400

    tic-tac-toe (full): make buttons align properly in Chrome

--- tic-tac-toe/full.js
+++ tic-tac-toe/full.js
@@ -1,1 +1,1 @@
-for(i=12;--i;d.write(i%4?`<input type=button style=width:30 onclick="value=d[${i*3>>2}]=value||'XO'[i++%2];for(a of[0,12,24,2,6,10,3,9])for(q of(b=a%4+1,c=d[a>>=2])&&c==d[a+=b]&c==d[a+b]?d.body.children:[])q.value=c">`:"<br>"))d=document
+for(i=12;--i;d.write(i%4?`<input type=button value=" "onclick="value-1?value=d[${i*3>>2}]='XO'[i++%2]:0;for(a of[0,12,24,2,6,10,3,9])for(q of(b=a%4+1,c=d[a>>=2])&&c==d[a+=b]&c==d[a+b]?d.body.children:[])q.value=c"style=width:30>`:`<br>`))d=document
